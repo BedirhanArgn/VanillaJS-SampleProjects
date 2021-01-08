49eb5225f2621dff0ffe72b7f5df970421b49bec
BedirhanArgn <argunbedir@gmail.com>
2021-01-08 21:40:29 +0300

    Mobil cihazlar icin menu tasarımı yapildi

--- 3-Atolye15-WebDesign/script.js
+++ 3-Atolye15-WebDesign/script.js
@@ -1,12 +1,27 @@
-window.onscroll = function()
-{
+window.onscroll = function() {
     var left = document.querySelector(".l-header");
 
-     console.log(document.documentElement.scrollTop)
+    console.log(document.documentElement.scrollTop)
     if (document.documentElement.scrollTop > 60) {
         left.classList.add("is-sticky");
-    } 
+    }
     if (document.documentElement.scrollTop === 0) {
         left.classList.remove("is-sticky");
-    } 
+    }
 }
+
+let el = document.querySelector('.hamburger-box');
+
+el.addEventListener("click", function() {
+    let navEl = document.querySelector('.navigation-container');
+
+    let hamburger = document.querySelector('.hamburger');
+    if (hamburger.classList.contains("is-active")) {
+        hamburger.classList.remove("is-active")
+        navEl.classList.remove("is-actives");
+    } else {
+        hamburger.classList.add("is-active");
+        navEl.classList.add("is-actives");
+    }
+
+})
